21453d5d2872d5748e1e128bb344f40793fd3092
Rudra Patel <rudrakiranpatel1185@gmail.com>
2026-05-05 19:55:38 +0530

    changed db connection method

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -9,17 +9,28 @@
 //   connectionString: databaseUrl,
 // });
 
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// module.exports = { pool }; // export the pool;
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
-module.exports = { pool }; // export the pool;
+module.exports = { pool };
 
 //
